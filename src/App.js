4d92e29b83bf2e5d4a3e7a2defdc76f5d20cc985
Aditya Raorane <145953001+AdityaRaorane22@users.noreply.github.com>
2025-10-25 17:12:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
-import Pricing from "./pages/Pricing";
-import Contact_Us from "./pages/Contact_Us";
 
 function App() {
   return (
@@ -16,8 +14,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard/*" element={<Dashboard />} />
-          <Route path="dashboard/pricing" element={<Pricing />} />
-          <Route path="dashboard/contact" element={<Contact_Us/>} />
         </Routes>
       </Router>
     </UserProvider>
